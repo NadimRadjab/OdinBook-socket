b409ddfcb478f1fb900566264fba0b459c339ed5
Nadim Radjab <nadimradjab94@gmail.com>
2021-09-27 00:13:10 +0200

    chat setup

---
+++ index.js
@@ -0,0 +1,40 @@
+const io = require("socket.io")(8900, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
+let users = [];
+
+const addUser = (userId, socketId) => {
+  !users.some((user) => user.userId === userId) &&
+    users.push({ userId, socketId });
+};
+const removeUser = (socketId) => {
+  users = users.filter((user) => user.socketId !== socketId);
+};
+const getUser = (senderId) => {
+  return users.find((user) => user.userId === senderId);
+};
+
+io.on("connection", (socket) => {
+  console.log("connected");
+  socket.on("addUser", (userId) => {
+    addUser(userId, socket.id);
+    io.emit("getUsers", users);
+  });
+  socket.on("sendMessage", ({ senderId, reciverId, message }) => {
+    const user = getUser(reciverId);
+
+    if (!user) return;
+    io.to(user.socketId).emit("getMessage", {
+      senderId,
+      message,
+    });
+  });
+
+  socket.on("disconnect", () => {
+    removeUser(socket.id);
+    io.emit("getUsers", users);
+  });
+});
